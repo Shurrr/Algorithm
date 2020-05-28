7ecdde63aa0fd18f079b8e9f9231b5940b787b23
赫 <43637704+Shurrr@users.noreply.github.com>
2020-05-29 00:47:25 +0900

    Create 2047. 신문 헤드라인.py

---
+++ 2047. 신문 헤드라인.py
@@ -0,0 +1,12 @@
+# smallheadline(String), capitalHeadline(String) 변수 생성/ 입력
+
+smallheadline = input()
+capitalHeadline = ''
+
+# Headline(String)을 대문자로 변환한다.(함수)
+
+capitalHeadline = smallheadline.upper()
+
+# 변환한 대문자 문자열을 출력한다.
+print(capitalHeadline)
+
